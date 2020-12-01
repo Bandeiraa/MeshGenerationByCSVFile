d2339e61c117daff18753b722a959ae930fbc0d6
Davi <frdavibandeira@hotmail.com>
2020-12-01 15:15:03 -0300

    Rebuilding meshs with new walls, but still with overlap

--- Assets/Data.cs
+++ Assets/Data.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Data 
 {
-    public string x1;
-    public string y1;
-    public string z1;
+    /*public int x1;
+    public int y1;
+    public int z1;
 
-    public string x2;
-    public string y2;
-    public string z2;
+    public int x2;
+    public int y2;
+    public int z2;
 
-    public string x3;
-    public string y3;
-    public string z3;
+    public int x3;
+    public int y3;
+    public int z3;
 
-    public string x4;
-    public string y4;
-    public string z4;
+    public int x4;
+    public int y4;
+    public int z4;
+    */
+    public int x1;
+    public int y1;
+    public int x2;
+    public int y2;
+
 }
--- Assets/readCSVFile.cs
+++ Assets/readCSVFile.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class readCSVFile : MonoBehaviour
 {
 
-    [SerializeField] float offset = 100;
+    [SerializeField] float offset = 30;
     Mesh mesh;
     List<Vector3> vertices;
     List<int> triangles;
@@ -17,7 +17,7 @@ public class readCSVFile : MonoBehaviour
     { 
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        TextAsset graphdata = Resources.Load<TextAsset>("Grafo");     
+        TextAsset graphdata = Resources.Load<TextAsset>("Graph");     
 
         string[] data = graphdata.text.Split(new char[] { '\n' });
 
@@ -28,49 +28,86 @@ public class readCSVFile : MonoBehaviour
             string[] row = data[i].Split(new char[]{','});
             
             Data d = new Data();
+            /*
             //X, Y and Z 1    
-            d.x1 = row[0];
-            d.y1 = row[1];
-            d.z1 = row[2];
+            int.TryParse(row[0], out d.x1);
+            int.TryParse(row[1], out d.y1);
+            int.TryParse(row[2], out d.z1);
 
             //X, Y and Z 2
-            d.x2 = row[3];
-            d.y2 = row[4];
-            d.z2 = row[5]; 
+            int.TryParse(row[3], out d.x2);
+            int.TryParse(row[4], out d.y2);
+            int.TryParse(row[5], out d.z2); 
 
             //X, Y and Z 3
-            d.x3 = row[6];
-            d.y3 = row[7];
-            d.z3 = row[8];
+            int.TryParse(row[6], out d.x3);
+            int.TryParse(row[7], out d.y3);
+            int.TryParse(row[8], out d.z3);
 
             //X, Y and Z 4
-            d.x4 = row[9];
-            d.y4 = row[10];
-            d.z4 = row[11];
+            int.TryParse(row[9], out d.x4);
+            int.TryParse(row[10], out d.y4);
+            int.TryParse(row[11], out d.z4);
+            */
+
+            int.TryParse(row[0], out d.x1);
+            int.TryParse(row[1], out d.y1);
+            int.TryParse(row[2], out d.x2);
+            int.TryParse(row[3], out d.y2);
 
             datas.Add(d);
+            
+            /*
+            Vector3 p1 = new Vector3(d.x1, 0, d.z1);
+            Vector3 p2 = new Vector3(d.x2, 0, d.z2);
+            Vector3 p3 = new Vector3(d.x3, 0, d.z3);
+            Vector3 p4 = new Vector3(d.x4, 0, d.z4);
+            */
 
-            //print(d.x1);
-            Vector3 offset1 = new Vector3(float.Parse(d.x1), 0, float.Parse(d.z1));
-            //Vector3 teste = new Vector3(d.x2, 0, d.z2);
-            print(offset1);
+            //print(p1);
+            //print(result);
+            
             //print(teste);
 
-            /*Vector3 x = new Vector3(d.p1, 0, d.p2);    
-            Vector3 x2 = new Vector3(d.p3, 0, d.p4);
+            Vector3 x = new Vector3(d.x1, 0, d.y1);
+            //print(x);    
+            Vector3 x2 = new Vector3(d.x2, 0, d.y2);
+            Vector3 aux = new Vector3(1, 0, 0);
             Vector3 targetDir = x - x2;
-            //print(targetDir);
 
-            Vector3 y = new Vector3(d.p1, 70, d.p2); 
-            Vector3 y2 = new Vector3(d.p3, 70, d.p4);
-            
+            Vector3 y = new Vector3(d.x1, 70, d.y1); 
+            Vector3 y2 = new Vector3(d.x2, 70, d.y2);
             Vector3 targetDirAux = y - y2;
-  
-            Vector3 result = Vector3.Cross(targetDir.normalized, Vector3.up) * offset;
-            print(result);
-            Vector3 resultAux = Vector3.Cross(targetDirAux.normalized, Vector3.up) * offset;
 
-            Vector3 negativeValue = x - result;
+            Vector3 result = Vector3.Cross(targetDir.normalized, Vector3.up) * offset;
+            Vector3 resultAux = Vector3.Cross(targetDirAux.normalized, Vector3.up) * offset;
+            Vector3 negativeOffset1 = x - result;
+            Vector3 positiveOffset1 = x + result;
+            Vector3 teste1 = negativeOffset1 + positiveOffset1;
+            Vector3 negativeOffset2 = x2 - result;
+            Vector3 positiveOffset2 = x2 + result;
+            Vector3 teste2 = negativeOffset2 + positiveOffset2;
+            Vector3 teste3 = teste1 - teste2;
+            Vector3 testeCrossP = Vector3.Cross(teste3.normalized, Vector3.up) * offset;
+            //print(teste);
+            /*Vector3 xAux = new Vector3(d.x1, 0, d.y1 - 10); 
+            Vector3 x2Aux = new Vector3(d.x2, 0, d.y2 - 10);
+            Vector3 y2aux = new Vector3(d.x1, 70, d.y1 - 10); 
+            Vector3 y2aux2 = new Vector3(d.x2, 70, d.y2 - 10);
+            Vector3 targetDirAuxX = xAux - x2Aux;
+            Vector3 targetDirAux2 = y2aux - y2aux2;
+
+
+
+            Vector3 resultx2Aux = Vector3.Cross(targetDirAuxX.normalized, Vector3.up) * offset;
+            Vector3 resulty2Aux = Vector3.Cross(targetDirAux2.normalized, Vector3.up) * offset;
+            */
+
+            //print(targetDirXAux);
+
+            //print(xAux);
+
+            /*Vector3 negativeValue = x - result;
             Vector3 positiveValue = x + result;
             Vector3 auxOffset = Vector3.Cross(result.normalized, Vector3.up) * offset;
             Vector3 auxOffsetPos = Vector3.Cross(positiveValue.normalized, Vector3.up) * offset;
@@ -78,34 +115,34 @@ public class readCSVFile : MonoBehaviour
 
             //Base Vertices
             
-            /*vertices.Add(d.p1);
-            vertices.Add(d.p2);
-            vertices.Add(d.p3);
-            vertices.Add(d.p4);
-            */
+            vertices.Add(x);
+            vertices.Add(x2);
+            vertices.Add(x + testeCrossP);
+            vertices.Add(x2 + testeCrossP);
+
             
-            /*
             //Top Vertices
-            vertices.Add(y - resultAux); 
-            vertices.Add(y2 - resultAux);
+            vertices.Add(y); 
+            vertices.Add(y2);
             vertices.Add(y + resultAux);
             vertices.Add(y2 + resultAux);
-            */
             
-            /*                            
+            
+            
+                                        
             //Chão 
-            triangles.Add(0 + (i - 1) * 4);
-            triangles.Add(1 + (i - 1) * 4);
-            triangles.Add(2 + (i - 1) * 4);
+            triangles.Add(0 + (i - 1) * 8);
+            triangles.Add(1 + (i - 1) * 8);
+            triangles.Add(2 + (i - 1) * 8);
             //1st triangle
-            triangles.Add(2 + (i - 1) * 4);
-            triangles.Add(1 + (i - 1) * 4);
-            triangles.Add(3 + (i - 1) * 4);    
+            triangles.Add(2 + (i - 1) * 8);
+            triangles.Add(1 + (i - 1) * 8);
+            triangles.Add(3 + (i - 1) * 8);    
             //2nd triangle
-            */
             
-            /*
-            //Chão 
+            
+            
+            /*//Chão 
             triangles.Add(2 + (i - 1) * 8);
             triangles.Add(3 + (i - 1) * 8);
             triangles.Add(0 + (i - 1) * 8);
@@ -114,6 +151,7 @@ public class readCSVFile : MonoBehaviour
             triangles.Add(3 + (i - 1) * 8);
             triangles.Add(1 + (i - 1) * 8);    
             //2nd triangle
+            */
             
             
             //Topo da Parede
@@ -165,17 +203,16 @@ public class readCSVFile : MonoBehaviour
             triangles.Add(6 + (i - 1) * 8);
             triangles.Add(7 + (i - 1) * 8); 
             //2nd triangle
-            */
+            
             
                
-            
         }
         //print(vertices.Count);
-        /*mesh.Clear();
+        mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals(); 
-        */
+        
 
         /*foreach(Vector3 v in vertices){
             Debug.Log("V: " + v.x + ", VY1: " + v.z);
