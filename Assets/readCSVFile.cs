ef935b1aa1a99279cb6480a5643ed356d657d1f5
Davi <frdavibandeira@hotmail.com>
2020-12-02 09:52:57 -0300

    Adding Comments and Gizmos to Visualize before play

--- Assets/readCSVFile.cs
+++ Assets/readCSVFile.cs
@@ -139,21 +139,7 @@ public class readCSVFile : MonoBehaviour
             triangles.Add(1 + (i - 1) * 8);
             triangles.Add(3 + (i - 1) * 8);    
             //2nd triangle
-            
-            
-            
-            /*//Chão 
-            triangles.Add(2 + (i - 1) * 8);
-            triangles.Add(3 + (i - 1) * 8);
-            triangles.Add(0 + (i - 1) * 8);
-            //1st triangle
-            triangles.Add(0 + (i - 1) * 8);
-            triangles.Add(3 + (i - 1) * 8);
-            triangles.Add(1 + (i - 1) * 8);    
-            //2nd triangle
-            */
-            
-            
+             
             //Topo da Parede
             triangles.Add(4 + (i - 1) * 8);
             triangles.Add(5 + (i - 1) * 8);
@@ -202,20 +188,85 @@ public class readCSVFile : MonoBehaviour
             triangles.Add(3 + (i - 1) * 8);
             triangles.Add(6 + (i - 1) * 8);
             triangles.Add(7 + (i - 1) * 8); 
-            //2nd triangle
-            
-            
+            //2nd triangle 
                
         }
-        //print(vertices.Count);
         mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
-        mesh.RecalculateNormals(); 
-        
+        mesh.RecalculateNormals();   
+    }
 
-        /*foreach(Vector3 v in vertices){
-            Debug.Log("V: " + v.x + ", VY1: " + v.z);
-        }*/   
+    private void OnDrawGizmos()
+    {   
+        //Gizmos - > Green Spheres are X and Z Initial and Final Points
+        //Gizmos - > Red Spheres and Lines(Always in Left) are values from negative offset of Green Spheres
+        //Gizmos - > Blue Spheres and Lines(Always in Right) are values from positive offset of Green Spheres
+
+            GetComponent<MeshFilter>().mesh = mesh;
+            TextAsset graphdata = Resources.Load<TextAsset>("Graph");     
+
+            string[] data = graphdata.text.Split(new char[] { '\n' });
+
+            for(int i = 1; i < data.Length - 1; i++) {
+                string[] row = data[i].Split(new char[]{','});
+
+                //Parsing CSV Values from string to int values
+
+                Data d = new Data();
+                int.TryParse(row[0], out d.x1);
+                int.TryParse(row[1], out d.y1);
+                int.TryParse(row[2], out d.x2);
+                int.TryParse(row[3], out d.y2);
+
+                //Creating Vectors with x, y and z values from the current CSV File Points
+
+                Vector3 x1 = new Vector3(d.x1, 0, d.y1); //Base X and Z First values
+                Vector3 x2 = new Vector3(d.x2, 0, d.y2); //Base X and Z Second values
+
+                Vector3 y1 = new Vector3(d.x1, 70, d.y1); //Top X and Z First values
+                Vector3 y2 = new Vector3(d.x2, 70, d.y2); //Top X and Z Second values
+                
+                Vector3 targetDirX = x1 - x2; // First - Second Base values to do Cross Product(Right hand rule)
+                Vector3 targetDirY = y1 - y2; // First - Second Top values to do Cross Product(Right hand rule)
+
+                Vector3 resultX = Vector3.Cross(targetDirX.normalized, Vector3.up) * offset; //Base Cross product to get an offset value
+                Vector3 resultY = Vector3.Cross(targetDirY.normalized, Vector3.up) * offset; //Top Cross product to get an offset value
+
+                //Creating Gizmos
+
+                Gizmos.color = Color.green; //Sphere Color
+                Gizmos.DrawSphere(x1, 1f); //Base X and Z First values Sphere Gizmos
+                Gizmos.DrawSphere(x2, 1f); //Base X and Z Second values Sphere Gizmos
+
+                Gizmos.DrawSphere(y1, 1f); //Top X and Z First values Sphere Gizmos
+                Gizmos.DrawSphere(y2, 1f); //Top X and Z Second values Sphere Gizmos                
+
+                Gizmos.color = Color.red; //Negative Spheres and Lines Color
+                
+                //Negative Base Offset Values
+                Gizmos.DrawSphere(x1 - resultX, 1f);
+                Gizmos.DrawSphere(x2 - resultX, 1f);
+                Gizmos.DrawLine  (x1 - resultX, x2 - resultX);
+                
+                
+                //Negative Top Offset Values
+                Gizmos.DrawSphere(y1 - resultY, 1f);
+                Gizmos.DrawSphere(y2 - resultY, 1f);
+                Gizmos.DrawLine  (y1 - resultY, y2 - resultY);
+
+                Gizmos.color = Color.blue; //Positive Spheres and Lines Color
+
+                //Positive Base Offset Values
+                Gizmos.DrawSphere(x1 + resultX, 1f);
+                Gizmos.DrawSphere(x2 + resultX, 1f);
+                Gizmos.DrawLine  (x1 + resultX, x2 + resultX);
+                
+                //Positive Top Offset Values
+                Gizmos.DrawSphere(y1 + resultY, 1f);
+                Gizmos.DrawSphere(y2 + resultY, 1f);
+                Gizmos.DrawLine(y1 + resultY, y2 + resultY);
+                
+            } 
     }
 }
